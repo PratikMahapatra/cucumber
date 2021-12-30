1eeff910bb29eedbed9d1db1e2ada00ec2a7dfd6
Pratik <pratikmahapatro2000@gmail.com>
2021-12-30 11:32:47 +0530

    sorry

---
+++ src/test/java/runner/TestRunner.java
@@ -0,0 +1,12 @@
+package runner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "resources", glue = "stepDefination")
+public class TestRunner {
+
+}
---
+++ src/test/java/stepDefination/StepDefination.java
@@ -0,0 +1,33 @@
+package stepDefination;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class StepDefination {
+	
+	@Given("User is on landing page")
+	public void user_is_on_landing_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    System.out.println("Sorry");
+	}
+
+	@When("User login into application with username and passwod")
+	public void user_login_into_application_with_username_and_passwod() {
+	    // Write code here that turns the phrase above into concrete actions
+		System.out.println("Sorry");
+	}
+
+	@Then("Home page is populated")
+	public void home_page_is_populated() {
+	    // Write code here that turns the phrase above into concrete actions
+		System.out.println("Sorry");
+	}
+
+	@Then("Cards are displayed")
+	public void cards_are_displayed() {
+	    // Write code here that turns the phrase above into concrete actions
+		System.out.println("Sorry");
+	}
+
+}
